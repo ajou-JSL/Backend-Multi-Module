0c06062fb3a5cb0b5811c2520357122130b2448d
Jaehyuk Chang <cjaehyuk4zed@gmail.com>
2024-11-20 04:35:04 +0900

    ✨ Add admin dashboard for teams, practiceRooms, performanceHalls

--- backend-module/src/main/java/jsl/moum/admin/controller/AdminController.java
+++ backend-module/src/main/java/jsl/moum/admin/controller/AdminController.java
@@ -3,7 +3,10 @@ package jsl.moum.admin.controller;
 import jsl.moum.admin.dto.AdminLoginRequest;
 import jsl.moum.admin.service.AdminService;
 import jsl.moum.auth.dto.MemberDto;
+import jsl.moum.business.dto.PerformanceHallDto;
+import jsl.moum.business.dto.PracticeRoomDto;
 import jsl.moum.global.response.ResultResponse;
+import jsl.moum.moum.team.dto.TeamDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -63,6 +66,53 @@ public class AdminController {
     @ResponseBody
     public MemberDto.Response getMemberDetails(@PathVariable int id) {
         return adminService.getMemberById(id);
+    }
+
+    @GetMapping("/team")
+    public String getTeamDashboard(Model model){
+        log.info("AdminController getTeamDashboard");
+
+        model.addAttribute("teamCount", adminService.getTeamCount());
+        model.addAttribute("teams", adminService.getTeams());
+        return "adminTeam";
+    }
+
+    @GetMapping("/team/view/{id}")
+    @ResponseBody
+    public TeamDto.Response getTeamDetails(@PathVariable int id) {
+        return adminService.getTeamById(id);
+    }
+
+    @GetMapping("/performance-hall")
+    public String getPerformanceHallDashboard(Model model){
+        log.info("AdminController getPerformanceHallDashboard");
+
+        model.addAttribute("performanceHallCount", adminService.getPerformanceHallCount());
+        model.addAttribute("performanceHalls", adminService.getPerformanceHalls());
+
+        return "adminPerformanceHall";
+    }
+
+    @GetMapping("/performance-hall/view/{id}")
+    @ResponseBody
+    public PerformanceHallDto getPerformanceHallDetails(@PathVariable int id) {
+        return adminService.getPerformanceHallById(id);
+    }
+
+    @GetMapping("/practice-room")
+    public String getPracticeRoomDashboard(Model model){
+        log.info("AdminController getPracticeRoomDashboard");
+
+        model.addAttribute("practiceRoomCount", adminService.getPracticeRoomCount());
+        model.addAttribute("practiceRooms", adminService.getPracticeRooms());
+
+        return "adminPracticeRoom";
+    }
+
+    @GetMapping("/practice-room/view/{id}")
+    @ResponseBody
+    public PracticeRoomDto getPracticeRoomDetails(@PathVariable int id) {
+        return adminService.getPracticeRoomById(id);
     }
 
     @GetMapping("/logout")
