d7e4eb6d16895832e22313b88bb9a890fd26179f
swlee <swo98@naver.com>
2024-11-21 18:23:26 +0900

    conflict

--- backend-module/src/main/java/jsl/moum/report/domain/TeamReport.java
+++ backend-module/src/main/java/jsl/moum/report/domain/TeamReport.java
@@ -2,6 +2,7 @@ package jsl.moum.report.domain;
 
 import jakarta.persistence.*;
 import jsl.moum.auth.domain.entity.MemberEntity;
+import jsl.moum.moum.team.domain.TeamEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,21 +13,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "member_report")
-public class MemberReport {
+@Table(name = "team_report")
+public class TeamReport {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    // Foreign key reference to the member
+    // Foreign key reference to the team
     @ManyToOne
-    @JoinColumn(name = "member_id", referencedColumnName = "id", nullable = false)
-    private MemberEntity member;
+    @JoinColumn(name = "team_id", referencedColumnName = "id", nullable = false)
+    private TeamEntity team;
 
-    // Storing the member's username directly
-    @Column(name = "member_username", nullable = false)
-    private String memberUsername;
+    // Storing the team name directly
+    @Column(name = "team_name", nullable = false)
+    private String teamName;
 
     // Foreign key reference to the reporter
     @ManyToOne
@@ -40,11 +41,11 @@ public class MemberReport {
     @Column(name = "type")
     private String type;
 
-    @Column(name = "reply")
-    private String reply;
-
     @Column(name = "details")
     private String details;
+
+    @Column(name = "reply")
+    private String reply;
 
     @Column(name = "is_resolved", columnDefinition = "boolean default false")
     private boolean isResolved;
