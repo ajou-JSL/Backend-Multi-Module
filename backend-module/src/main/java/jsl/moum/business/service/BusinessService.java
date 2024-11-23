e961ae5f9e357ca17fd64923516d667982a217b3
Jaehyuk Chang <cjaehyuk4zed@gmail.com>
2024-11-23 23:01:46 +0900

    🎨 Add detailed error codes

--- backend-module/src/main/java/jsl/moum/business/service/BusinessService.java
+++ backend-module/src/main/java/jsl/moum/business/service/BusinessService.java
@@ -6,6 +6,8 @@ import jsl.moum.business.domain.PracticeRoom;
 import jsl.moum.business.domain.PracticeRoomRepository;
 import jsl.moum.business.dto.PerformanceHallDto;
 import jsl.moum.business.dto.PracticeRoomDto;
+import jsl.moum.global.error.ErrorCode;
+import jsl.moum.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,13 +23,13 @@ public class BusinessService {
 
     public PracticeRoomDto getPracticeRoomById(int id){
         PracticeRoom room = practiceRoomRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 연습실 정보가 존재하지 않습니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.PRACTICE_ROOM_NOT_FOUND));
         return new PracticeRoomDto(room);
     }
 
     public PerformanceHallDto getPerformanceHallById(int id){
         PerformanceHall hall = performanceHallRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 공연장 정보가 존재하지 않습니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.PERFORMANCE_HALL_NOT_FOUND));
         return new PerformanceHallDto(hall);
     }
 }
