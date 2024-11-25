88af1ceb00cde774f997e45a4da535b37e1858a4
Jaehyuk Chang <cjaehyuk4zed@gmail.com>
2024-11-26 04:08:06 +0900

    ✨ Add APIs to delete moum rooms and halls

--- backend-module/src/main/java/jsl/moum/moum/lifecycle/domain/LifecyclePerformanceHallRepository.java
+++ backend-module/src/main/java/jsl/moum/moum/lifecycle/domain/LifecyclePerformanceHallRepository.java
@@ -1,6 +1,7 @@
 package jsl.moum.moum.lifecycle.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,4 +11,12 @@ public interface LifecyclePerformanceHallRepository extends JpaRepository<Lifecy
 
     @Query("SELECT l FROM LifecyclePerformanceHall l WHERE l.moum.id = :moumId")
     List<LifecyclePerformanceHall> findAllByMoumId(@Param("moumId") int moumId);
+
+    @Modifying
+    @Query("DELETE FROM LifecyclePerformanceHall l WHERE l.moum.id = :moumId")
+    void deleteAllByMoumId(@Param("moumId") int moumId);
+
+    @Modifying
+    @Query("DELETE FROM LifecyclePerformanceHall l WHERE l.moum.id = :moumId AND l.id = :performanceHallId")
+    void deleteByMoumIdAndPerformanceHallId(@Param("moumId") int moumId, @Param("performanceHallId") int performanceHallId);
 }
