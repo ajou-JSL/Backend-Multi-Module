02dd25dab411140652243264424b0a23d9fe77f3
swlee <swo98@naver.com>
2024-11-26 16:53:07 +0900

    docker-compose 파일 변경

--- backend-module/src/main/java/jsl/moum/MoumApplication.java
+++ backend-module/src/main/java/jsl/moum/MoumApplication.java
@@ -2,12 +2,25 @@ package jsl.moum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 public class MoumApplication {
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(MoumApplication.class, args);
+//    }
+
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:application-release.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(MoumApplication.class, args);
+
+        new SpringApplicationBuilder(MoumApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);;
     }
 
 }
