e7d9d0fb963532cc24a19cd2712307aff4a94d63
swlee <swo98@naver.com>
2024-12-03 15:33:28 +0900

    conntection 테스트용 api 작성

--- backend-module/src/main/java/jsl/moum/config/SecurityConfig.java
+++ backend-module/src/main/java/jsl/moum/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/test","/", "/join","/reissue","/send-mail","verify-code","/re-join","/test").permitAll()
+                        .requestMatchers("/login", "/test/**","/", "/join","/reissue","/send-mail","verify-code","/re-join","/api/chat/**","/api/test/**").permitAll()
                         .requestMatchers("/storage/upload").permitAll()
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("*/favicon.ico").permitAll()
